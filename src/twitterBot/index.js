581fb2534bef50afd7172db94ca5656da2975d2d
Reed Dunkle <reeddunkle@gmail.com>
2017-06-11 14:55:46 -0400

    WIP: added express

--- src/twitterBot/index.js
+++ src/twitterBot/index.js
@@ -1,7 +1,14 @@
+import express from 'express';
 import flow from 'lodash/fp/flow';
 import noop from 'lodash/noop';
 import Twitter from 'twitter';
 import { addApproval, cropText, decodeHTML, isAuthor } from '../tweetUtil';
+import { onTweetPosted } from './util';
+
+// HEROKU SETUP
+const app = express();
+app.get('/', (req, res) => { res.send('The robot is happily running.'); });
+app.listen(process.env.PORT || 5000);
 
 const config = {
   consumer_key: process.env.BOT_CONSUMER_KEY,
@@ -10,7 +17,7 @@ const config = {
   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
 };
 
-const users = {
+const USERS = {
   dtJR: 39344374,
   test: 62298196,
 };
@@ -18,11 +25,11 @@ const users = {
 const client = new Twitter(config);
 const path = 'statuses/filter';
 const params = {
-  follow: users.dtJR,
+  follow: USERS.dtJR,
 };
 
 function postTweet(status) {
-  client.post('statuses/update', { status }, noop);
+  client.post('statuses/update', { status }, onTweetPosted);
 }
 
 function seekApproval(tweet) {
